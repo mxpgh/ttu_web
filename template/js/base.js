0015593b2e8816467109452e8f0ae6dfbe4929b8
mxpgh <panh1@sohu.com>
2019-02-19 16:42:16 +0800

    Add files via upload

--- template/js/base.js
+++ template/js/base.js
@@ -19,6 +19,11 @@ $("#status").click(function(event){
     window.location.href = "/status/index";
 })
 
+$("#upload").click(function(event){
+    event.preventDefault();
+    window.location.href = "/upload/index";
+})
+
 function del_cookie(name)
 {
     document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/;';
@@ -35,4 +40,29 @@ $("form[data-type=formAction]").submit(function(event){
             location.href = $(target).attr("form-redirect");
         }
     },"json")
+})
+
+$("#uploadform").submit(function(event){
+    event.preventDefault();
+    var target = event.target;
+    var action = $(target).attr("action");
+    var data = new FormData($("#uploadform")[0]);
+
+    $.ajax({
+        url         : action,
+        data        : data,
+        cache       : false,
+        contentType : false,
+        processData : false,
+        type        : "POST",
+        dataType    : "json",
+        enctype     : "multipart/form-data",
+        success     :  function(ret){
+            if(ret.Ret == "0") {
+                alert(ret.Reason);
+            } else {
+               location.href = $(target).attr("form-redirect");
+            }
+        }
+    });
 })
