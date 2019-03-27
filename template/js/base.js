9783370cc0e4013933f3e0edde9d2fb6629bfef2
mxpgh <panh1@sohu.com>
2019-03-27 16:02:32 +0800

    Add files via upload

--- template/js/base.js
+++ template/js/base.js
@@ -34,6 +34,11 @@ $("#passwd").click(function(event){
     window.location.href = "/passwd/index";
 })
 
+$("#sysmgr").click(function(event) {
+    event.preventDefault();
+    window.location.href = "/sysmgr/index";
+})
+
 function del_cookie(name)
 {
     document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;path=/;';
